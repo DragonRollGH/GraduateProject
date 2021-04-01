a57dd9483175e9db0c3b6f67b88925e1cdcb07a3
DragonRollGH <492782447@qq.com>
2021-04-02 00:26:49 +0800

    web

---
+++ html/ADC/index.js
@@ -0,0 +1,25 @@
+isLight = false;
+
+function adj(value) {
+    if (value > 0) {
+        document.getElementById("opt").innerHTML = `+${value}`;
+    } else {
+        document.getElementById("opt").innerHTML = `${value}`;
+    }
+}
+
+function toggle() {
+    isLight = !isLight;
+    if (isLight) {
+        document.getElementById("Light").style = "color: rgb(255,200,0);"
+        ctx.fillRect(0, 0, 300, 300);
+    } else {
+        document.getElementById("Light").style = "color: gray;"
+        ctx.clearRect(0, 0, 300, 300);
+    }
+}
+
+onload = function () {
+    ctx = document.getElementById("Canvas").getContext("2d");
+    ctx.fillStyle = "rgb(255,200,0)";
+}
