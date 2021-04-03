13afe70b6644c0d72fa46d2b839bc94f3cf62257
DragonRollGH <492782447@qq.com>
2021-04-03 22:20:05 +0800

    ok

--- server/static/light.js
+++ server/static/light.js
@@ -1,26 +1,28 @@
-isLight = false;
+power = false;
 
 
 function toggle() {
-    isLight = !isLight;
-    if (isLight) {
+    power = !power;
+    if (power) {
         document.getElementById("Light").style = "color: rgb(255,200,0);"
         ctx.fillRect(0, 0, 300, 300);
+        Request.open('GET', `http://127.0.0.1:5000/api?d=light&m=toggle&a=1`, true);
+        Request.send();
     } else {
         document.getElementById("Light").style = "color: gray;"
         ctx.clearRect(0, 0, 300, 300);
+        Request.open('GET', `/api?d=light&m=toggle&a=0`, true);
+        Request.send();
     }
 }
 
 function offset(value) {
-    if (value > 0) {
-        document.getElementById("opt").innerHTML = `+${value}`;
-    } else {
-        document.getElementById("opt").innerHTML = `${value}`;
-    }
+    Request.open('GET', `/api?d=light&m=offset&a=${value}`, true);
+    Request.send();
 }
 
 onload = function () {
     ctx = document.getElementById("Canvas").getContext("2d");
     ctx.fillStyle = "rgb(255,200,0)";
+    Request = new XMLHttpRequest();
 }
