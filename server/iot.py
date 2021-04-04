ee1856fc555f86669ed98d0fc0d2808de787f307
DragonRollGH <492782447@qq.com>
2021-04-04 18:47:55 +0800

    add curtain

--- server/iot.py
+++ server/iot.py
@@ -11,7 +11,8 @@ class IoT:
         self.mqtt.loop_start()
 
         self.device = {
-            'light': Light(self)
+            'light': Light(self),
+            'curtain': Curtain(self)
         }
 
         self.sensor = {
@@ -72,11 +73,45 @@ class Light:
         self.value = round((value - MIN) * 24 / (MAX - MIN))
         self.publish()
 
-
 class Curtain:
-    pass
+    def __init__(self, iot):
+        self.iot = iot
+        self.method = {
+            'offset': self.offset,
+            'toggle': self.toggle
+        }
+        self.power = 1
+        self.value = 12
+        self.offsetx = 12
 
-class Sensorlight:
+    def publish(self):
+        if self.power is 0:
+            self.iot.mqtt.publish('curtain', '0')
+            print('[curtain] 0')
+        else:
+            value = self.value + self.offsetx - 12
+            value = 0 if value < 0 else value
+            value = 24 if value > 24 else value
+            self.iot.mqtt.publish('curtain', str(value))
+            print('[curtain] ' + str(value))
+
+    def toggle(self, value):
+        self.power = int(value)
+        self.publish()
+
+    def offset(self, value):
+        self.offsetx = int(value)
+        self.publish()
+
+    def sensor(self, value):
+        MIN = 50
+        MAX = 400
+        value = MIN if value < MIN else value
+        value = MAX if value > MAX else value
+        self.value = round((value - MIN) * 24 / (MAX - MIN))
+        self.publish()
+
+class SensorLight:
     def __init__(self, iot):
         self.iot = iot
         self.value = 0
@@ -85,4 +120,5 @@ class Sensorlight:
     def update(self, value):
         self.value = int(value)
         self.iot.device['light'].sensor(self.value)
+        self.iot.device['curtain'].sensor(self.value)
 
