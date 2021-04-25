520cdbe7291cac2b972cfd9a0dac92d01aacb2cd
DragonRollGH <492782447@qq.com>
2021-04-25 21:15:25 +0800

    js

--- server/static/history.js
+++ server/static/history.js
@@ -1,46 +1,41 @@
 function drawCharts(values) {
-
+    let charts = {
+        light: ['light',],
+    };
+    let colors = [
+        'red',
+        'blue',
+    ]
+    let labels = values.time;
+    for (let chart in charts) {
+        let ctx = document.getElementById(chart).getContext("2d");
+        let datasets = [];
+        for (let i in charts[chart]) {
+            datasets.push({
+                data: values[charts[chart][i]],
+                label: charts[chart][i],
+                borderColor: colors[i],
+                spanGaps: true,
+                lineTension: 0.5,
+            });
+        }
+        new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: labels,
+                datasets: datasets,
+            },
+        });
+    }
 }
 
 onload = function () {
-    devices = [
-        'light',
-    ];
-    datas = [];
-    requests = [];
-    for (let i in devices) {
-        requests.push(new XMLHttpRequest());
-        requests[i].open('GET', `/api/history?d=${devices[i]}`, true);
-        requests[i].send()
-        requests[i].onreadystatechange = ()=>{
-            if (requests[i].readyState == 4) {
-                datas.push(requests[i].responseText)
-                console.log(requests[i].responseText);
-            }
-        }
-    }
-    var request = new XMLHttpRequest();
+    let request = new XMLHttpRequest();
     request.open("GET", '/api/history');
     request.onreadystatechange = ()=>{
         if (request.readyState == 4) {
-            values = JSON.parse(request.responseText);
+            let values = JSON.parse(request.responseText);
             drawCharts(values);
         }
     }
-    ctx = document.getElementById("light").getContext("2d");
-    // var my = new Chart(ctx, {
-    //     type: "line",
-    //     data: [0,10,20,10,30,0],
-    //     options: {}
-    // });
-    new Chart(ctx, {
-        type: 'line',
-        data: {
-            labels: 'asdasdasf',
-            datasets: [{
-                data: [10, 20, 30, 10, 50, 60],
-                borderColor: 'red',
-            }],
-        },
-    });
 }
